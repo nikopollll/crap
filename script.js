ca6d8e8eff7ed19dd710fcf1ad0417d5bd61d08e
Niko <mr.shariman@yandex.ru>
2022-07-13 17:08:52 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+let number = 5;
+let string = 'Hello!';
+let sym = Symbol();
+let booolean = true;    
+null; // not exist
+undefined; // нет значения
+let obj = {}; // coolection of data and functiont
+
+
+
+console.log(4/0);
+
+let something;
+console,console.log(something);
+
+let preson = {
+    name: 'Jhon',
+    age: 25,
+    isMarried: false
+}
+
+console.log(preson['name']); // объект и его действия
+
+let arr = ['plum.png','orang.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert('Hello World!');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', "");
+
+// console.log(typeof(answer));
+
+// console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isCheked = false,
+    isClose = false;
+
+console.log(isCheked || !isClose);
